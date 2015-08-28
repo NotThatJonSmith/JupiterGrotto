95047d28b06437c1cbabff84a88e6ae2760461b5
Jonathan Smith <jnthsmth@umich.edu>
2015-08-28 02:01:43 -0400

    Style fix
    Reconfigured for Good Braces

--- JupiterGrotto/main.cpp
+++ JupiterGrotto/main.cpp
@@ -10,39 +10,38 @@ void CreateGround(b2World& World, float X, float Y);
 /** Create the boxes */
 void CreateBox(b2World& World, int MouseX, int MouseY);
 
-int main()
-{
-	/** Prepare the window */
+int main() {
+
 	sf::RenderWindow Window(sf::VideoMode(800, 600, 32), "Test");
 	Window.setFramerateLimit(60);
 
-	/** Prepare the world */
 	b2Vec2 Gravity(0.f, 9.8f);
 	b2World World(Gravity);
 	CreateGround(World, 400.f, 500.f);
 
-	/** Prepare textures */
 	sf::Texture GroundTexture;
 	sf::Texture BoxTexture;
 	GroundTexture.loadFromFile("res/ground.png");
 	BoxTexture.loadFromFile("res/box.png");
 
-	while (Window.isOpen())
-	{
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-		{
+	while (Window.isOpen()) {
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 			int MouseX = sf::Mouse::getPosition(Window).x;
 			int MouseY = sf::Mouse::getPosition(Window).y;
 			CreateBox(World, MouseX, MouseY);
 		}
-		World.Step(1 / 60.f, 8, 3);
 
+		World.Step(1 / 60.f, 8, 3);
 		Window.clear(sf::Color::White);
 		int BodyCount = 0;
-		for (b2Body* BodyIterator = World.GetBodyList(); BodyIterator != 0; BodyIterator = BodyIterator->GetNext())
-		{
-			if (BodyIterator->GetType() == b2_dynamicBody)
-			{
+
+		for (b2Body* BodyIterator = World.GetBodyList();
+
+			 BodyIterator != 0;
+			 BodyIterator = BodyIterator->GetNext()) {
+
+			if (BodyIterator->GetType() == b2_dynamicBody) {
 				sf::Sprite Sprite;
 				Sprite.setTexture(BoxTexture);
 				Sprite.setOrigin(16.f, 16.f);
@@ -50,9 +49,7 @@ int main()
 				Sprite.setRotation(BodyIterator->GetAngle() * 180 / b2_pi);
 				Window.draw(Sprite);
 				++BodyCount;
-			}
-			else
-			{
+			} else {
 				sf::Sprite GroundSprite;
 				GroundSprite.setTexture(GroundTexture);
 				GroundSprite.setOrigin(400.f, 8.f);
@@ -61,19 +58,17 @@ int main()
 				Window.draw(GroundSprite);
 			}
 		}
+
 		Window.display();
 	}
-
 	return 0;
 }
 
-void CreateBox(b2World& World, int MouseX, int MouseY)
-{
+void CreateBox(b2World& World, int MouseX, int MouseY) {
 	b2BodyDef BodyDef;
 	BodyDef.position = b2Vec2(MouseX / SCALE, MouseY / SCALE);
 	BodyDef.type = b2_dynamicBody;
 	b2Body* Body = World.CreateBody(&BodyDef);
-
 	b2PolygonShape Shape;
 	Shape.SetAsBox((32.f / 2) / SCALE, (32.f / 2) / SCALE);
 	b2FixtureDef FixtureDef;
@@ -83,13 +78,11 @@ void CreateBox(b2World& World, int MouseX, int MouseY)
 	Body->CreateFixture(&FixtureDef);
 }
 
-void CreateGround(b2World& World, float X, float Y)
-{
+void CreateGround(b2World& World, float X, float Y) {
 	b2BodyDef BodyDef;
 	BodyDef.position = b2Vec2(X / SCALE, Y / SCALE);
 	BodyDef.type = b2_staticBody;
 	b2Body* Body = World.CreateBody(&BodyDef);
-
 	b2PolygonShape Shape;
 	Shape.SetAsBox((800.f / 2) / SCALE, (16.f / 2) / SCALE);
 	b2FixtureDef FixtureDef;
